233f75703da482ac821c295d40aa8f61a5775989
TabTang <jiangtao213@163.com>
2017-03-05 11:08:38 +0800

    add redux-thunk scss

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/Main';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore,applyMiddleware } from 'redux';
+import thunk from 'redux-thunk'
 //
 import reducer from './reducers';
 
-const store = createStore(reducer);
+const store = createStore(reducer,applyMiddleware(thunk));
 
 
 
